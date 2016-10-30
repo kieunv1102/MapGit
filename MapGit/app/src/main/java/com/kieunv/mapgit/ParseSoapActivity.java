590ebff39fcc09b4f7d2b606c86444500bab0c93
kieunv <kieu.nv.itfuns@gmail.com>
2016-10-30 23:03:23 +0700

    soapui

--- MapGit/app/src/main/java/com/kieunv/mapgit/ParseSoapActivity.java
+++ MapGit/app/src/main/java/com/kieunv/mapgit/ParseSoapActivity.java
@@ -4,26 +4,38 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 
 public class ParseSoapActivity extends AppCompatActivity {
 
+    private static final String NAMESPACE = "http://tempuri.org/";
+    private static final String METHOD_NAME = "Login";
+    private static final String SOAP_ACTION = "http://tempuri.org/IMobileService/Login";
+    private static final String URL = "http://203.190.173.22:9002/MobileService.svc?wsdl";
+    private String response;
+    private TextView txtResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parse_soap);
-        new pay().execute();
+        txtResponse = (TextView)findViewById(R.id.txtResponse);
+//        new pay().execute();
+        new GetObjectService().execute();
     }
 
     class pay extends AsyncTask<Void, Void, Void> {
@@ -38,10 +50,7 @@ public class ParseSoapActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... unused) {
 
-            final String NAMESPACE = "http://tempuri.org/";
-            final String METHOD_NAME = "Login";
-            final String SOAP_ACTION = "http://tempuri.org/Login";
-            final String URL = "http://203.190.173.22:9002/MobileService.svc?wsdl";
+
 
 
             SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
@@ -85,6 +94,52 @@ public class ParseSoapActivity extends AppCompatActivity {
             if (this.dialog.isShowing()) {
                 this.dialog.dismiss();
             }
+        }
+    }
+
+    private class GetObjectService extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+            request.addProperty("TypeClient", "1");
+            request.addProperty("ClientVersion", "1.0");
+            request.addProperty("Username", "sysadmin");
+            request.addProperty("Password", "123456");
+
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.setOutputSoapObject(request);
+
+            HttpTransportSE httpTransport = new HttpTransportSE(URL);
+
+            httpTransport.debug = true;
+            try {
+                httpTransport.call(SOAP_ACTION, envelope);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } //send request
+            SoapObject result = null;
+            try {
+                result = (SoapObject) envelope.getResponse();
+//                Log.e("result", result.toString());
+            } catch (SoapFault e) {
+                e.printStackTrace();
+            }
+            response = "ID: " + result.getProperty(0).toString() + ", Name: " + result.getProperty(1).toString() + ", Action: " + result.getProperty(2).toString() + ", Description: " + result.getProperty(3).toString() + ", Locale key: " + result.getProperty(4).toString();
+            Log.e("Object", "" + response);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            txtResponse.setText(response);
         }
     }
 
